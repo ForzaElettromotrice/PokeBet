4cd9503eb43dda618e469381fab44ee968342197
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-11-04 11:44:56 +0100

    added type supremacy

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,6 +4,44 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+TABLE_TYPE = {
+    "Normal": ([], ["Rock", "Steel"], ["Ghost"]),
+    "Fire": (["Grass", "Ice", "Bug", "Steel"],
+             ["Fire", "Water", "Rock", "Dragon"], []),
+    "Water": (["Fire", "Ground", "Rock"],
+              ["Water", "Grass", "Dragon"], []),
+    "Electric": (["Water", "Flying"],
+                 ["Electric", "Grass", "Dragon"], ["Ground"]),
+    "Grass": (["Water", "Ground", "Rock"],
+              ["Fire", "Grass", "Poison", "Flying", "Bug", "Dragon", "Steel"], []),
+    "Ice": (["Grass", "Ground", "Flying", "Dragon"],
+            ["Fire", "Water", "Ice", "Steel"], []),
+    "Fighting": (["Normal", "Ice", "Rock", "Dark", "Steel"],
+                 ["Poison", "Flying", "Psychic", "Bug", "Fairy"], []),
+    "Poison": (["Grass", "Fairy"],
+               ["Poison", "Ground", "Rock", "Ghost"], []),
+    "Ground": (["Fire", "Electric", "Poison", "Rock", "Steel"],
+               ["Grass", "Bug"], ["Flying"]),
+    "Flying": (["Grass", "Fighting", "Bug"],
+               ["Electric", "Rock", "Steel"], []),
+    "Psychic": (["Fighting", "Poison"],
+                ["Psychic", "Steel"], ["Dark"]),
+    "Bug": (["Grass", "Psychic", "Dark"],
+            ["Fire", "Fighting", "Poison", "Flying", "Ghost", "Steel", "Fairy"], []),
+    "Rock": (["Fire", "Ice", "Flying", "Bug"],
+             ["Fighting", "Ground", "Steel"], []),
+    "Ghost": (["Psychic", "Ghost"],
+              ["Dark"], ["Normal"]),
+    "Dragon": (["Dragon"],
+               ["Steel"], ["Fairy"]),
+    "Dark": (["Psychic", "Ghost"],
+             ["Fighting", "Dark", "Fairy"], []),
+    "Steel": (["Ice", "Rock", "Fairy"],
+              ["Fire", "Water", "Electric", "Steel"], []),
+    "Fairy": (["Fighting", "Dragon", "Dark"],
+              ["Fire", "Poison", "Steel"], [])
+}
+
 def count_fnt(data: dict) -> Tuple[int, int]:
     out1 = 0
     out2 = 0
@@ -29,6 +67,61 @@ def calculate_mean_hp(data: dict):
 
     return p1_mean, p2_mean
 
+def calculate_score(p1: set[str], p2: set[str]) -> Tuple[int, int]:
+    adv = 0
+    res = 0
+    for t1 in p1:
+        t1 = t1.capitalize()
+        for t2 in p2:
+            t2 = t2.capitalize()
+
+            if t2 in TABLE_TYPE[t1][0]:
+                adv += 1
+            elif t2 in TABLE_TYPE[t1][1]:
+                adv -= 1
+            elif t2 in TABLE_TYPE[t1][2]:
+                adv -= 2
+
+            if t1 in TABLE_TYPE[t2][0]:
+                res -= 1
+            elif t1 in TABLE_TYPE[t2][1]:
+                res += 1
+            elif t1 in TABLE_TYPE[t2][2]:
+                res += 2
+    return adv, res
+
+def calculate_type_supremacy(data: dict) -> Tuple[int, int]:
+    p1_pokemons = { }
+    p2_pokemons = { }
+
+    for turn in data["battle_timeline"]:
+
+        p1_name = turn["p1_pokemon_state"]["name"]
+        p2_name = turn["p2_pokemon_state"]["name"]
+
+        if p1_name not in p1_pokemons:
+            p1_pokemons[p1_name] = set()
+        if p2_name not in p2_pokemons:
+            p2_pokemons[p2_name] = set()
+
+        if turn["p1_move_details"] is not None:
+            p1_pokemons[p1_name].add(turn["p1_move_details"]["type"])
+        if turn["p2_move_details"] is not None:
+            p2_pokemons[p2_name].add(turn["p2_move_details"]["type"])
+
+        if turn["p1_pokemon_state"]["status"] == "fnt":
+            del p1_pokemons[p1_name]
+        if turn["p2_pokemon_state"]["status"] == "fnt":
+            del p2_pokemons[p2_name]
+
+    p1_points = [[0, 0] for _ in p1_pokemons]
+    for i, p1 in enumerate(p1_pokemons.values()):
+        for p2 in p2_pokemons.values():
+            adv, res = calculate_score(p1, p2)
+            p1_points[i][0] += adv
+            p1_points[i][1] += res
+    return sum(x[0] for x in p1_points), sum(x[1] for x in p1_points)
+
 def create_features(data: list[dict]) -> pd.DataFrame:
     feature_list = []
     for battle in tqdm(data, desc = "Extracting features"):
@@ -52,6 +145,8 @@ def create_features(data: list[dict]) -> pd.DataFrame:
         p1_hps, p2_hps = calculate_mean_hp(battle)
         features['p1_mean_hp_pct'] = p1_hps
         features['p2_mean_hp_pct'] = p2_hps
+
+        features["p1_type_adv_supremacy"], features["p1_type_res_supremacy"] = calculate_type_supremacy(battle)
 
         features['battle_id'] = battle.get('battle_id')
         if 'player_won' in battle:
